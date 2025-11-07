5c47c68bfee5ada4ca7640f5b88dd8a54831bd1f
Lakshya Garg <garglakshya00@gmail.com>
2025-11-07 13:00:04 +0530

    updtaed

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "SafePaws - Protecting Stray Dogs and Our Community",
+  description: "Report issues related to street dogs and connect with NGOs to take action. View reports in your location and help create a safer environment for everyone.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
